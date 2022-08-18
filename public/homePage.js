4a481bf84b8d3c083d8bee3e28bdfc7e34d9a67d
Anastasiia Korchagina <anastasia_92_92@list.ru>
2022-08-18 18:03:54 +0300

    first commit

---
+++ public/homePage.js
@@ -0,0 +1,15 @@
+const logoutButton = new LogoutButton();
+
+logoutButton.action = ()=>{
+  ApiConnector.logout((response)=>{
+    if(response.success){
+      location.reload();
+    }
+    });
+};
+
+ApiConnector.current = ((response)=>{
+  if(response.success){
+    ProfileWidget.showProfile(response.data);
+  }
+});
