111c7865bc6e742acb97899ed3be6bf42f708533
Anastasiia Korchagina <anastasia_92_92@list.ru>
2022-08-22 22:55:59 +0300

    added RatesBoard, ProfoleWidget, MoneyManager, FavoriteWidges

--- public/homePage.js
+++ public/homePage.js
@@ -13,3 +13,18 @@ ApiConnector.current = ((response)=>{
     ProfileWidget.showProfile(response.data);
   }
 });
+
+const ratesBoard = new RatesBoard();
+
+const getStocksFunc = function(){
+  ApiConnector.getStocks((response) => {
+    if(response.success){
+      ratesBoard.clearTable();
+      ratesBoard.fillTable(response.data);
+      }
+    });
+}
+
+getStocksFunc();
+setInterval(getStocksFunc, 60000);
+
--- public/loginPage.js
+++ public/loginPage.js
@@ -22,3 +22,73 @@ userForm.loginFormCallback = (data) => {
      }
    });
  };
+
+const moneyManager = new MoneyManager();
+
+moneyManager.addMoneyCallback = (data) => {
+  ApiConnector.addMoney (data,(response) => {
+    if(response.success){
+      ProfileWidget.showProfile(response.data)
+    }
+    const message = response.error === undefined ? "Счет успешно пополнен" : response.error;
+    moneyManager.setMessage(response.success, message);
+  });
+};
+
+moneyManager.conversionMoneyCallback = (data) => {
+  ApiConnector.convertMoney(data,(response) =>{
+    if(response.success){
+      ProfileWidget.showProfile(response.data)
+    }
+    const message = response.error === undefined ? "Конвертирование успешно выполнено" : response.error;
+    moneyManager.setMessage(response.success, message);
+  });
+};
+
+moneyManager.sendMoneyCallback = (data) => {
+  ApiConnector.transferMoney(data,(response) => {
+    if(response.success){
+      ProfileWidget.showProfile(response.data)
+    }
+    const message = response.error === undefined ? "Перевод успешно выполнен" : response.error;
+    moneyManager.setMessage(response.success, message);
+  })
+};
+
+const favoritesWidget = new FavoritesWidget();
+
+ApiConnector.getFavorites((response) => {
+   if (response.success) {
+     favoritesWidget.clearTable();
+     favoritesWidget.fillTable(response.data);
+     moneyManager.updateUsersList(response.data);
+   }
+ });
+
+favoritesWidget.addUserCallback = (data) => {
+  ApiConnector.addUserToFavorites(data, (response) => {
+    if (response.success) {
+    favoritesWidget.clearTable();
+    favoritesWidget.fillTable(response.data);
+    moneyManager.updateUsersList(response.data);
+    }
+
+    const message = response.error === undefined ? "Пользователь успешно добавлен" : response.error;
+    favoritesWidget.setMessage(response.success, message);
+  });
+};
+
+favoritesWidget.removeUserCallback = (data) => {
+  ApiConnector.removeUserFromFavorites(data, (response) => {
+    if (response.success) {
+      favoritesWidget.clearTable();
+      favoritesWidget.fillTable(response.data);
+      moneyManager.updateUsersList(response.data);
+    }
+
+    const message = response.error === undefined ? "Пользователь успешно удален" : response.error;
+    favoritesWidget.setMessage(response.success, message);
+  });
+};
+
+
